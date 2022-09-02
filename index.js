2436f5295b6535f026410ec7e72a0816952f2c7a
SirJimKe <jimmymutuku1@gmail.com>
2022-09-02 11:00:16 +0300

    Added renderCard function on index.js

--- index.js
+++ index.js
@@ -16,11 +16,32 @@ document.addEventListener('DOMContentLoaded', () => {
 
 });
 
+const columns = document.querySelector('.columns');
 const heart = document.querySelector('.heart');
 const likes = document.querySelector('.likes');
 
 
-function renderCard(){
-    
+function renderCard(place){
+    let card = document.createElement('div');
+    card.className = "card";
+    card.innerHTML = `
+        <h4>${place.description}</h4>
+        <div class="card-image">
+            <img class="image" src="${place.images}">
+        </div>
+        <div class="reviews">
+            <p class="likes">${place.likes} likes<span class="heart">&nbsp  ❤️</span></p>
+            <a href="#"><i id="share" class="fa fa-share-nodes fa-lg"></i></a>
 
+            <form>
+                <input class="comments" name="comment" value="" type="text" placeholder="Add a review...">
+                <button type="submit" class="submit">Post</button>
+            </form>
+        </div>
+        <div class="review-holder">
+            <p>${place.reviews}</p>
+        </div>
+    `   
+    columns.appendChild(card);
 }
+
